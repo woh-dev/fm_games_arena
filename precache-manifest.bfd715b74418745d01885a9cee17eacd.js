0133c63f8b9f3e58d1328ee421c93abd6508d50c
Chris Wohlers <chriswohlers@gmail.com>
2019-05-12 23:24:00 -0700

    Updates

---
+++ precache-manifest.bfd715b74418745d01885a9cee17eacd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a35d566ab222a640cdc20901adc748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e0ccf9b5ffa68af2096",
+    "url": "/static/css/main.e7ff6fe9.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "0e0ccf9b5ffa68af2096",
+    "url": "/static/js/main.a2ef2d44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
